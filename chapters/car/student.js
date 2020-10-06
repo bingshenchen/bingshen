77a92aaedcc1f9c0d3337481ed6aa0e9447cd7ba
bingshenchen <cbs2315453671@gmail.com>
2020-10-06 15:00:35 +0200

    BOODSCHAP

--- chapters/car/student.js
+++ chapters/car/student.js
@@ -208,3 +208,85 @@ function zigZag(car){
     forward(car);
     firstLeft(car);
 }
+
+function forwardUntilFreeRight(car) {
+    while (sensorRight(car)) {
+        forward(car);
+    }
+}
+
+function secondRight(car){
+    forwardUntilFreeRight(car);
+    forward(car);
+    forwardUntilFreeRight(car);
+    turnRight(car);
+    forwardUntilWall(car);
+}
+
+function thirdRight(car){
+    for(var i = 1; i<=2; i++){
+        forwardUntilFreeRight(car);
+        forward(car);
+    }
+    forwardUntilFreeRight(car);
+    turnRight(car);
+    forwardUntilWall(car);
+}
+
+
+function forwardUntilNthRight(car, nrights) {
+    let i = nrights;
+
+    while (i > 0) {
+        forward(car);
+
+        if (!sensorRight(car)) {
+            i = i - 1;
+        }
+    }
+}
+
+function fourthRight(car) {
+    forwardUntilNthRight(car, 4);
+    turnRight(car);
+    forwardUntilWall(car);
+}
+
+function forwardUntilNthLeft(car, nrights) {
+    let i = nrights;
+
+    while (i > 0) {
+        forward(car);
+
+        if (!sensorLeft(car)) {
+            i = i - 1;
+        }
+    }
+}
+
+function fifthLeft(car){
+    forwardUntilNthLeft(car, 5);
+    turnLeft(car);
+    forwardUntilWall(car);
+} 
+
+function maze(car) {
+    function L(n) {
+        forwardUntilNthLeft(car, n);
+        turnLeft(car);
+    }
+
+    function R(n) {
+        forwardUntilNthRight(car, n);
+        turnRight(car);
+    }
+
+    R(2);
+    L(1);
+    L(2);
+    L(2);
+    R(4);
+    R(1);
+    L(3);
+    forwardUntilWall(car);
+}
